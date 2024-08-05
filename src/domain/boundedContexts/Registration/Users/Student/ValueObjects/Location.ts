85736fece4bf4795565a5daf944f95fb6ff679b8
Muniabi <infotkahn@yandex.ru>
2024-08-05 11:16:07 +0300

    Сделал valueObjects

---
+++ src/domain/boundedContexts/Registration/Users/Student/ValueObjects/Location.ts
@@ -0,0 +1,21 @@
+import { ValueObject } from "./ValueObject";
+
+interface ILocation {
+    value: string;
+}
+
+class Location extends ValueObject<ILocation> {
+    private constructor(props: ILocation) {
+        super(props);
+    }
+
+    public static create(location: string): Location {
+        if (!location || location.trim().length === 0) {
+            throw new Error("Необходимо выбрать местоположение.");
+        }
+
+        return new Location({ value: location });
+    }
+}
+
+export { Location };
