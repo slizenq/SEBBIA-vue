b8c504f26f07ebfa38f55b5ecb3324c7e761a826
Muniabi <infotkahn@yandex.ru>
2024-08-05 17:06:52 +0300

    Add export in Direction

--- src/domain/boundedContexts/Registration/Users/Student/ValueObjects/Direction.ts
+++ src/domain/boundedContexts/Registration/Users/Student/ValueObjects/Direction.ts
@@ -1,24 +1,23 @@
 import { ValueObject } from "./ValueObject";
 
-
 interface IDirection {
-    value: string
+    value: string;
 }
 
 class Direction extends ValueObject<IDirection> {
-
-    private constructor (props: IDirection) {
+    private constructor(props: IDirection) {
         super(props);
     }
-    public static create (direction: string) : Direction {
-        
-        if (direction === undefined || direction === null || direction.length > 50) {
-            throw new Error('Пустое или слишком длинное значение для специальности');
+    public static create(direction: string): Direction {
+        if (direction === undefined || direction === null) {
+            throw new Error(
+                "Пустое или слишком длинное значение для специальности"
+            );
         } else {
-            return new Direction({value : direction});
+            return new Direction({ value: direction });
         }
     }
 }
 
-export {Direction};
-// 
+export { Direction };
+//
