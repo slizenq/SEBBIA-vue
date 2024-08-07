1520a0d0093c6c9cc0c63bc4b23c347d06cd6389
SuperPuperKatushka <katrinsokolova05@gmail.com>
2024-08-07 10:37:08 +0300

    imports

--- src/domain/boundedContexts/Request/ValueObjects/Uuid.ts
+++ src/domain/boundedContexts/Request/ValueObjects/Uuid.ts
@@ -2,23 +2,23 @@ import { ValueObject } from "~/src/domain/Templates/ValueObject";
 import { v4 as uuidv4, validate as uuidValidate } from "uuid";
 
 interface IUuidProps {
-    value: string;
+  value: string;
 }
 
 export class Uuid extends ValueObject<IUuidProps> {
-    private constructor(props: IUuidProps) {
-        super(props);
+  private constructor(props: IUuidProps) {
+    super(props);
+  }
+
+  public static create(uuid: string): Uuid {
+    if (!uuid) {
+      uuid = uuidv4(); // Генерация нового UUID, если не предоставлен
     }
 
-    public static create(uuid?: string): Uuid {
-        if (!uuid) {
-            uuid = uuidv4(); // Генерация нового UUID, если не предоставлен
-        }
-
-        if (!uuidValidate(uuid)) {
-            throw new Error("Неверный формат UUID.");
-        }
-
-        return new Uuid({ value: uuid });
+    if (!uuidValidate(uuid) && uuidValidate) {
+      throw new Error("Неверный формат UUID.");
+    } else {
+      return new Uuid({ value: uuid });
     }
+  }
 }
