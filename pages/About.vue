44bc8f372da6116ccb36576fa92c68329ccb1e6f
slizenq <159460129+slizenq@users.noreply.github.com>
2024-08-16 13:07:22 +0300

    test

--- pages/About.vue
+++ pages/About.vue
@@ -1,18 +1,29 @@
 <template>
     <div class="container">
-        <div class="BreadCrumb">
-            <BreadCrumb/>
+        <div class="BreadCrumb"><BreadCrumb/></div>
+        <div class="Search"><Search/></div>
+        <div class="DropdownList"><DropdownList/></div>
+        <div class="company__list">
+            <div v-for="item in 9" :key="item">
+                <Card/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import BreadCrumb from '~/components/BreadCrumb.vue';
+import Card from '~/components/Card.vue';
+import DropdownList from '~/components/UI/DropdownList.vue';
+import Search from '~/components/UI/Search.vue';
 
 
 export default {
     components: {
         BreadCrumb,
+        Search,
+        DropdownList,
+        Card,
     },
 };
 </script>
