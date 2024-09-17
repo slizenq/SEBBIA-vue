ffa7426e5dc8e2d6cffae139dd46dd29371961fb
Muniabi <infotkahn@yandex.ru>
2024-09-17 16:59:35 +0300

    сделай модельное окно

--- pages/Vacancy.vue
+++ pages/Vacancy.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="container">
-        <BreadCrumb/>
+        <BreadCrumb />
         <div class="company__vacancy" @search="sea(message)">
             <div class="company__vacancy__left-part">
                 <div class="left__element">
                     <h1 class="direction">{{ vacancyData.description }}</h1>
-                    <p class="description">Мобильный разработчик Фулл-стек разработчик Бекенд разработчик</p>
+                    <p class="description">
+                        Мобильный разработчик Фулл-стек разработчик Бекенд
+                        разработчик
+                    </p>
                 </div>
                 <div class="left__element">
                     <p class="element__title">Чем предстоит заниматься</p>
                     <ul>
-                        <li>Это команда профессионалов, стремящаяся к решению сложных задач</li>
+                        <li>
+                            Это команда профессионалов, стремящаяся к решению
+                            сложных задач
+                        </li>
                         <li>Это команда профессионалов</li>
                         <li>Это команда профессионалов, стремящаяся к</li>
-                        <li>Это команда профессионалов, стремящаяся к решению </li>
+                        <li>
+                            Это команда профессионалов, стремящаяся к решению
+                        </li>
                     </ul>
                 </div>
                 <div class="left__element">
@@ -32,63 +40,166 @@
                 </div>
                 <div class="left__element">
                     <p class="element__title">О проектах</p>
-                    <p class="description">Это команда профессионалов, стремящаяся к решению сложных задач Это команда профессионалов, стремящаяся к решению сложных задач Это команда профессионалов, стремящаяся к решению сложных задач Это...</p>
+                    <p class="description">
+                        Это команда профессионалов, стремящаяся к решению
+                        сложных задач Это команда профессионалов, стремящаяся к
+                        решению сложных задач Это команда профессионалов,
+                        стремящаяся к решению сложных задач Это...
+                    </p>
                 </div>
             </div>
             <div class="company__vacancy__right-part">
                 <div class="about__company">
                     <div>
                         <div>
-                            <p class="right__title text__align">{{ vacancyData.title }}<img src="./../assets/images/vacancy/confirm.svg"></p>
+                            <p class="right__title text__align">
+                                {{ vacancyData.title
+                                }}<img
+                                    src="./../assets/images/vacancy/confirm.svg"
+                                />
+                            </p>
                             <p class="right__description">ООО</p>
                         </div>
                         <div class="wrap__margin">
-                            <p class="right__description text__align"><img src="./../assets/images/vacancy/location.svg">{{ vacancyData.location }}</p>
-                            <p class="right__description text__align"><img src="./../assets/images/vacancy/date.svg">22.02.2011</p>
+                            <p class="right__description text__align">
+                                <img
+                                    src="./../assets/images/vacancy/location.svg"
+                                />{{ vacancyData.location }}
+                            </p>
+                            <p class="right__description text__align">
+                                <img
+                                    src="./../assets/images/vacancy/date.svg"
+                                />22.02.2011
+                            </p>
                         </div>
                     </div>
-                    <img src="./../assets/images/company-logo.svg" alt="logo">
+                    <img src="./../assets/images/company-logo.svg" alt="logo" />
                 </div>
-                <p class="company__description">Это команда профессионалов, стремящаяся к решению сложных задач. Мы облао команда профессионалов, стремящаяся к решению сложных задач. Мы обла... </p>
-                <ElButton class="respond" type="primary" v-if="checkCompany">Откликнуться<img src="./../assets/images/vacancy/micro-link.svg"></ElButton>
+                <p class="company__description">
+                    Это команда профессионалов, стремящаяся к решению сложных
+                    задач. Мы облао команда профессионалов, стремящаяся к
+                    решению сложных задач. Мы обла...
+                </p>
+                <ElButton
+                    @click="openDialog"
+                    class="respond"
+                    type="primary"
+                    v-if="checkCompany"
+                    >Откликнуться<img
+                        src="./../assets/images/vacancy/micro-link.svg"
+                /></ElButton>
+                <el-dialog
+                    v-model="isDialogVisible"
+                    :title="titleModal"
+                    width="480px"
+                    :close-on-click-modal="true"
+                    :close-on-press-escape="true"
+                    @close="resetDialog"
+                    class="login-dialog"
+                >
+                    <div class="wrap">
+                        <div class="wrap__title">
+                            <el-icon
+                                ><InfoFilled color="#409EFF" height="22px"
+                            /></el-icon>
+                            <h2>Заполните резюме</h2>
+                        </div>
+                        <div class="wrap__content">
+                            <p class="wrap__content-text">
+                                Для отклика требуется создать резюме в личном
+                                кабинете, где есть подсказки, чтобы научиться
+                                профессионально о себе рассказать будущему
+                                работодателю
+                            </p>
+                        </div>
+                        <div class="wrap__footer">
+                            <el-button @click="closeDialog">Позже</el-button>
+                            <el-button @click="link" type="primary"
+                                >Перейти к резюме</el-button
+                            >
+                        </div>
+                    </div>
+                </el-dialog>
                 <div class="partner">
                     <p class="contract">Партнеры</p>
                 </div>
             </div>
-        </div> 
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ElButton, ElTag } from 'element-plus';
-import { IP } from '~/components/UI/auth/Authentication';   
-import { ref } from 'vue';
+import { ElButton, ElTag, ElDialog } from "element-plus";
+import { InfoFilled } from "@element-plus/icons-vue";
+import { IP } from "~/components/UI/auth/Authentication";
+import { ref } from "vue";
 import axios from "axios";
+
+const isDialogVisible = ref(false);
+const openDialog = () => {
+    isDialogVisible.value = true;
+};
+
+const closeDialog = () => {
+    isDialogVisible.value = false;
+};
+
+const link = () => {
+    navigateTo("/account/resume/ResumeStudent");
+};
 
 const checkCompany = ref(true);
 const vacancyData = ref({});
 const fetchVacancyData = async () => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (user.is_company == true) {
         checkCompany.value = false;
     } else {
         checkCompany.value = true;
     }
     console.log(checkCompany.value);
-    
-    const response = await axios.get(`${IP}/vacancies/${localStorage.getItem("vacancy")}`);
+
+    const response = await axios.get(
+        `${IP}/vacancies/${localStorage.getItem("vacancy")}`
+    );
     vacancyData.value = response.data;
     // vacancyRequirements.value = response.data.requirements
 };
-fetchVacancyData()
+fetchVacancyData();
 </script>
 
 <style scoped>
-
 .text__align {
     display: flex;
     align-items: center;
-    gap: 5px
+    gap: 5px;
+}
+
+.wrap__title {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 8px;
+    margin-bottom: 14px;
+}
+
+.wrap__content {
+    padding-block: 10px;
+    margin-bottom: 14px;
+}
+.wrap__content-text {
+    font-size: 16px;
+    line-height: 157%;
+}
+
+.wrap__footer {
+    display: flex;
+    justify-content: end;
+    gap: 8px;
+}
+
+.wrap {
+    padding: 10px 5px;
 }
 .skills {
     margin-top: 15px;
@@ -99,7 +210,7 @@ fetchVacancyData()
     margin-right: 8px;
 }
 .partner {
-    margin-top: 15px
+    margin-top: 15px;
 }
 .contract {
     font-weight: 700;
@@ -107,16 +218,17 @@ fetchVacancyData()
 }
 .respond {
     width: 100%;
-    height: 44px
+    height: 44px;
 }
 ul {
-    list-style-type: none; 
+    list-style-type: none;
 }
-ul li::before {  
-    content: "—";  
-    margin-right: 5px; 
+ul li::before {
+    content: "—";
+    margin-right: 5px;
 }
-.company__description, .respond {
+.company__description,
+.respond {
     margin-top: 15px;
 }
 .wrap__margin {
@@ -124,7 +236,7 @@ ul li::before {
 }
 .right__title {
     font-size: 22px;
-    font-weight: 700
+    font-weight: 700;
 }
 .about__company {
     display: flex;
@@ -148,15 +260,15 @@ ul li::before {
 }
 .element__title {
     font-weight: 700;
-    font-size: 17px
+    font-size: 17px;
 }
 .left__element {
-    margin-top: 42px
+    margin-top: 42px;
 }
 .description,
 ul > li {
     color: #303133;
-    margin-top: 15px
+    margin-top: 15px;
 }
 .right__description {
     margin-top: 8px;
