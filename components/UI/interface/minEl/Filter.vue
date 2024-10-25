62f327442e7b61e6df4d586832bdc874a6f2d27c
slizenq <159460129+slizenq@users.noreply.github.com>
2024-10-25 14:41:44 +0300

    delete kakashka

--- components/UI/interface/minEl/Filter.vue
+++ components/UI/interface/minEl/Filter.vue
@@ -1,22 +1,8 @@
 <template>
-    <el-button type="primary" :icon="Filter" plain @click="signup">Фильтр</el-button>
+    <el-button type="primary" :icon="Filter" plain>Фильтр</el-button>
 </template>
 
 <script setup>
 import { ElButton } from "element-plus";
 import { Filter } from "@element-plus/icons-vue";
-import axios from "axios";
-
-const signup = async () => {
-  try {
-    const response = await axios.post('http://localhost:3000/signup', {
-      email: "viktor.rak101@gmail.com",
-      password: "ShaddowFixend123!",
-      isCompany: false
-    });
-    console.log("Response:", response.data);
-  } catch (error) {
-    console.error("Error during signUp:", error);
-  }
-};
 </script>
