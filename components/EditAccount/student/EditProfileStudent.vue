f69678e0a513af6d6205e8047b272c04a315d4fa
Muniabi <infotkahn@yandex.ru>
2024-11-14 13:50:48 +0300

    дата у студента передается

--- components/EditAccount/student/EditProfileStudent.vue
+++ components/EditAccount/student/EditProfileStudent.vue
@@ -4,48 +4,114 @@
             <div>
                 <p class="description popUp">
                     Добавить фото
-                    <el-popover placement="top-start" title="Добавляй свое фото" :width="350" trigger="hover" content="Это гарантирует лучшее запоминание твоего резюме среди других претендентов">
-                        <template #reference><el-icon><InfoFilled /></el-icon></template>
+                    <el-popover
+                        placement="top-start"
+                        title="Добавляй свое фото"
+                        :width="350"
+                        trigger="hover"
+                        content="Это гарантирует лучшее запоминание твоего резюме среди других претендентов"
+                    >
+                        <template #reference
+                            ><el-icon><InfoFilled /></el-icon
+                        ></template>
                     </el-popover>
                 </p>
-                <ElUpload><img src="./../../../assets/images/addPhoto.svg" class="img_margin"></ElUpload>
+                <ElUpload
+                    ><img
+                        src="./../../../assets/images/addPhoto.svg"
+                        class="img_margin"
+                /></ElUpload>
             </div>
             <div class="fio">
                 <p class="description">Имя</p>
-                <el-input v-model="first_name" style="width: 100%" class="fio__input"placeholder="Имя"/>
+                <el-input
+                    v-model="first_name"
+                    style="width: 100%"
+                    class="fio__input"
+                    placeholder="Имя"
+                />
                 <p class="description">Фамилия</p>
-                <el-input v-model="last_name" style="width: 100%" class="fio__input" placeholder="Фамилия"/>
+                <el-input
+                    v-model="last_name"
+                    style="width: 100%"
+                    class="fio__input"
+                    placeholder="Фамилия"
+                />
                 <p class="description">Отчество</p>
-                <el-input v-model="middle_name" style="width: 100%" class="fio__input" placeholder="Отчесво"/>
+                <el-input
+                    v-model="middle_name"
+                    style="width: 100%"
+                    class="fio__input"
+                    placeholder="Отчесво"
+                />
             </div>
             <div class="dropdown-list">
                 <div>
                     <p class="description">Город</p>
-                    <el-select-v2 class="dropdown" v-model="city" :options="cityOptions" placeholder="Выберите город" style="width: 240px; margin-right: 16px; vertical-align: middle;" clearable/>
+                    <el-select-v2
+                        class="dropdown"
+                        v-model="city"
+                        :options="cityOptions"
+                        placeholder="Выберите город"
+                        style="
+                            width: 240px;
+                            margin-right: 16px;
+                            vertical-align: middle;
+                        "
+                        clearable
+                    />
                 </div>
                 <div>
                     <p class="description">Учебное заведение</p>
-                    <el-select-v2 class="dropdown" v-model="education" :options="educationOptions" placeholder="Выберите учебное заведение"style="width: 240px; margin-right: 16px; vertical-align: middle;"clearable/>
+                    <el-select-v2
+                        class="dropdown"
+                        v-model="education"
+                        :options="educationOptions"
+                        placeholder="Выберите учебное заведение"
+                        style="
+                            width: 240px;
+                            margin-right: 16px;
+                            vertical-align: middle;
+                        "
+                        clearable
+                    />
                 </div>
             </div>
             <div>
                 <p class="description">Дата рождения</p>
-                <el-date-picker class="dropdown" v-model="born_date" type="date" placeholder="ээ пупупууу" :size="size" style="width: 100%; margin-top: 8px"/>
+                <el-date-picker
+                    class="dropdown"
+                    v-model="born_date"
+                    type="date"
+                    placeholder="ээ пупупууу"
+                    :size="size"
+                    style="width: 100%; margin-top: 8px"
+                />
             </div>
-            <ElButton type="primary" @click="handleFormSubmit" class="send_form">Сохранить</ElButton>
-        </div>   
+            <ElButton type="primary" @click="handleFormSubmit" class="send_form"
+                >Сохранить</ElButton
+            >
+        </div>
     </div>
 </template>
 
 <script setup>
 import { Picture as IconPicture, InfoFilled } from "@element-plus/icons-vue";
-import { ElUpload, ElIcon, ElInput, ElSelectV2, ElDatePicker, ElButton, ElPopover } from "element-plus";
+import {
+    ElUpload,
+    ElIcon,
+    ElInput,
+    ElSelectV2,
+    ElDatePicker,
+    ElButton,
+    ElPopover,
+} from "element-plus";
 import { ref, computed, defineEmits } from "vue";
 import { sendForm as sendFormHandler } from "../EditStudent";
 
 import { ElNotification } from "element-plus";
 
-const photo = ref('test');
+const photo = ref("test");
 const first_name = ref(null);
 const middle_name = ref(null);
 const last_name = ref(null);
@@ -53,9 +119,19 @@ const city = ref(null);
 const education = ref(null);
 const born_date = ref(null);
 const showUpProgress = ref(false);
-const emit = defineEmits(['profileUpdated', 'updateDialogRedactor']);
-const cities = ["Москва", "Санкт-Петербург", "Ростов-на-Дону", "Челябинск", "Новосибирск", "Нальчик", "Орёл", "Якутск", "Красонодар", "Зерноград"];
-
+const emit = defineEmits(["profileUpdated", "updateDialogRedactor"]);
+const cities = [
+    "Москва",
+    "Санкт-Петербург",
+    "Ростов-на-Дону",
+    "Челябинск",
+    "Новосибирск",
+    "Нальчик",
+    "Орёл",
+    "Якутск",
+    "Красонодар",
+    "Зерноград",
+];
 
 const dialogRedactor = ref(false);
 const cityOptions = cities.map((city, idx) => ({
@@ -69,51 +145,74 @@ const educationOptions = educationInstit.map((institution, idx) => ({
     label: institution,
     class: "custom-option",
 }));
-const selectedCity = computed(() => cityOptions.find(option => option.value === city.value) || null);
-const selectedEducation = computed(() => educationOptions.find(option => option.value === education.value) || null);
+const selectedCity = computed(
+    () => cityOptions.find((option) => option.value === city.value) || null
+);
+const selectedEducation = computed(
+    () =>
+        educationOptions.find((option) => option.value === education.value) ||
+        null
+);
+
+function formatBornDate(date) {
+    if (date instanceof Date) {
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    } else if (typeof date === "string") {
+        const [year, month, day] = date.split("-");
+        return `${day}.${month}.${year}`;
+    } else {
+        throw new TypeError("Invalid date format");
+    }
+}
 
 const handleFormSubmit = async () => {
+    const formattedBornDate = formatBornDate(born_date.value);
     const isSuccessful = await sendFormHandler(
-        first_name, 
-        last_name, 
-        middle_name, 
-        selectedEducation, 
-        selectedCity, 
-        photo, 
-        showUpProgress, 
-        dialogRedactor 
+        first_name,
+        last_name,
+        middle_name,
+        selectedEducation,
+        selectedCity,
+        photo,
+        formattedBornDate,
+        showUpProgress,
+        dialogRedactor
     );
-    emit('updateDialogRedactor', false);
+    emit("updateDialogRedactor", false);
     if (isSuccessful) {
         ElNotification({
-            title: "Успешно отправлено", 
-            duration: 1500, 
-            type: "success", 
-            showClose: true
+            title: "Успешно отправлено",
+            duration: 1500,
+            type: "success",
+            showClose: true,
         });
     } else {
         ElNotification({
-            title: "Произошла ошибка при отправке", 
+            title: "Произошла ошибка при отправке",
             message: "Проверьте правильно ли вы заполнили все данные",
-            duration: 1500, 
-            type: "error", 
-            showClose: true
+            duration: 1500,
+            type: "error",
+            showClose: true,
         });
     }
-    emit('profileUpdated', { 
-        first_name: first_name.value, 
-        last_name: last_name.value, 
-        middle_name: middle_name.value, 
-        city: selectedCity.value?.label, 
-        education: selectedEducation.value?.label, 
-        photo: photo.value 
+    emit("profileUpdated", {
+        first_name: first_name.value,
+        last_name: last_name.value,
+        middle_name: middle_name.value,
+        city: selectedCity.value?.label,
+        education: selectedEducation.value?.label,
+        born_date: formattedBornDate,
+        photo: photo.value,
     });
 };
 </script>
 
 <style>
 .img_margin {
-    margin-top: 20px
+    margin-top: 20px;
 }
 .el-notification {
     width: 484px;
@@ -168,7 +267,7 @@ const handleFormSubmit = async () => {
 }
 .form {
     width: 550px;
-    margin-bottom: 10px
+    margin-bottom: 10px;
 }
 .demo-image__error .block[data-v-13a429be][data-v-13a429be] {
     padding: 30px 0;
